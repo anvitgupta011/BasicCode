51d614e3b04d4a562127d583f57f91cf33329fcf
Anvit <anvit_g@cs.iitr.ac.in>
2023-09-30 23:51:09 +0530

    Version 1.0 : Basic template files ready for java, cpp, html and css code

---
+++ index.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"Code started now"<<endl;
+    int x = 90;
+    int y = 89;
+    if(x<y){
+        cout<<x-y<<endl;
+    }
+    else{
+        cout<<y-x<<endl;
+    }
+    cout<<"Code completed"<<endl;
+    return 0;
+}
---
+++ index.java
@@ -0,0 +1,6 @@
+class A {
+    public static main void(String args[]){
+        int x = 89.8;
+        System.out.println(x);
+    }
+}
